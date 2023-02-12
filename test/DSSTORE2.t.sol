963de2e6202b3cfce12f07984fb0609776aeb686
smarsx <sammarshall333@gmail.com>
2023-02-12 12:48:32 -0600

    init

--- test/DSSTORE2.t.sol
+++ test/DSSTORE2.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.17;
 
-import { Test, console2 as console } from "lib/forge-std/src/Test.sol";
+import { Test } from "lib/forge-std/src/Test.sol";
 import { DSSTORE2 } from "../src/DSSTORE2.sol";
 
 // must abuse the setUp function to test self destruct
